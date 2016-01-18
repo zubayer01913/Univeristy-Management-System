1404b949f159c97b25d78f7f670ed4e6311e436c
Zubayer Bin Ayub <zubayer01913@gmail.com>
2016-01-19 02:12:13 +0600

    Course Assign To Teacher half 1

---
+++ UniversityManagementSystem/UniversityManagementSystem/Controllers/CourseToTeacherAssignController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UniversityManagementSystem.Models;
+using UniversityManagementSystem.ViewModels;
+
+namespace UniversityManagementSystem.Controllers
+{
+    public class CourseToTeacherAssignController : Controller
+    {
+        private UniveristyContext db = new UniveristyContext();
+
+        // GET: CourseToTeacherAssign
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CourseToTeacherAssign/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CourseToTeacherAssign/Create
+        public ActionResult Assign()
+        {
+            ViewBag.Department = new SelectList(db.Depatments, "Id", "Name");
+
+ 
+
+            return View();
+        }
+
+        // POST: CourseToTeacherAssign/Create
+        [HttpPost]
+        public ActionResult Assign(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CourseToTeacherAssign/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CourseToTeacherAssign/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CourseToTeacherAssign/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CourseToTeacherAssign/Delete/5
+        [HttpPost]
+        public ActionResult GetTeacherList(int id)
+        {
+            var list = db.Teachers.Where(x => x.DepartmentId == id).Select(x => new { value= x.Id, Text= x.TeacherName,credit= x.CreditToBeTaken}).ToList();         
+
+            return Json(list);
+        }
+    }
+}
---
+++ UniversityManagementSystem/UniversityManagementSystem/ViewModels/CourseAssignToTeacher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UniversityManagementSystem.ViewModels
+{
+    public class CourseAssignToTeacher
+    {
+        public int Id { get; set; }
+        public int DepatmentId { get; set; }
+        public int TeacherId { get; set; }
+        public double CreditCanBeTaken { get; set; }
+        public double RemainingCredit { get; set; }
+        public int CourseId { get; set; }
+        
+    }
+}
